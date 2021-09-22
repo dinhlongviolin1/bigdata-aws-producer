b3bed285494bef6191c93926177f6dfa5932e08e
dinhlongnguyen <dinhlongviolin1@gmail.com>
2021-09-22 16:22:53 +0700

    update pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "apps": [{
+        "name": "ProducerCrypto",
+        "script": "./main.py",
+        "args": [""],
+        "interpreter" : "pipenv",
+        "interpreter_args": "run python"
+    }]
+}
